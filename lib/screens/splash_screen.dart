b17f9910414d16d921ca986abb5673b0c857dc70
glone <glone0506@gmail.com>
2025-05-30 17:05:42 +0900

    Feat: Splash 페이지 추가

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -54,12 +54,55 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF3B4453),
-      body: Center(
-        child: Image.asset(
-          'assets/images/woori_card_splash.jpg',
-          width: MediaQuery.of(context).size.width * 0.7,
-          fit: BoxFit.contain,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Spacer(), // 상단 여백
+
+            // 중앙 아이콘 + 원큐PAY 텍스트
+
+            Align(
+              alignment: Alignment.center,
+              child: Transform.translate(
+                offset: const Offset(0, 70),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/icons/app_icon.png',
+                      width: 80,
+                      height: 80,
+                    ),
+                    const Text(
+                      '원큐PAY',
+                      style: TextStyle(
+                        fontFamily: 'WooridaumB',
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            const Spacer(), // 중앙과 하단 사이 여백
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Transform.translate(
+                  offset: const Offset(0, 35),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 0),
+                    child: Image.asset(
+                      'assets/images/woori_card_splash.png',
+                      width: MediaQuery.of(context).size.width * 0.85,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                )),
+          ],
         ),
       ),
     );
